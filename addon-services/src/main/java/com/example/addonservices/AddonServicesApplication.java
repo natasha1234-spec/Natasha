97b7db240d9c7310182fcb94d726fa8a86bf76ca
natasha1234-spec <natashapatel123457@gmail.com>
2024-12-19 16:37:46 +0530

    Create AddonServicesApplication.java

---
+++ addon-services/src/main/java/com/example/addonservices/AddonServicesApplication.java
@@ -0,0 +1,12 @@
+package com.smartparkingspots.addonservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddonServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddonServicesApplication.class, args);
+    }
+}
